70aba7a6f6381c68f9c1c2608003b917451dc46b
ThanhPhong <55240303+ThanhPhongIT@users.noreply.github.com>
2021-04-28 16:51:34 +0700

    style code input

--- src/app/qr-code/qr-code.page.ts
+++ src/app/qr-code/qr-code.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 import { BehaviorSubject } from 'rxjs';
+import { InputCodeModalComponent } from './input-code-modal/input-code-modal.component';
 
 @Component({
   selector: 'app-qr-code',
@@ -9,25 +11,30 @@ import { BehaviorSubject } from 'rxjs';
 export class QrCodePage implements OnInit {
   itemDetail = new BehaviorSubject<any>(null);
   dataQR: any;
-  constructor() {}
-  ngOnInit(): void {}
+  constructor(public modalController: ModalController) {}
+  ngOnInit(): void {
+  }
   // clearResult(): void {
   //   this.itemDetail = null;
   // }
 // resultString: string
+
+async presentModal() {
+  const modal = await this.modalController.create({
+    component: InputCodeModalComponent,
+    cssClass: 'input-code-modal',
+    backdropDismiss: true
+  });
+  modal.onDidDismiss().then((res)=>{
+    console.log(res.data);
+    
+  })
+  return await modal.present();
+}
   onCodeResult(ev) {
     this.dataQR = ev
     console.log(this.dataQR);
     console.log(ev);
-    
-
-    
-   
-
-    
-    
-
-
     // console.log(resultString);
     // const data = {
     //   value: resultString,
